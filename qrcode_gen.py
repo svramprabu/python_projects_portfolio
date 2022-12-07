fe1bb38399150dcd18e4b981c1f37b65c70f373d
svramprabu <111267339+svramprabu@users.noreply.github.com>
2022-12-07 07:36:16 +0530

    Add files via upload

---
+++ qrcode_gen.py
@@ -0,0 +1,18 @@
+import qrcode
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+to_be_generated= input ("Enter the content to generate QR")
+generate_qrcode(to_be_generated)
